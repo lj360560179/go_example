d024e4c9187654ac1311566a2c3edc1140af1295
lj360560179 <360560179@qq.com>
2018-01-12 14:15:40 +0800

    go study

--- model/TProduct.go
+++ model/TProduct.go
@@ -6,33 +6,33 @@ import (
 
 type TProduct struct {
 	Guid string `gorm:"column:guid" json:"guid"`
-	Sellerid string `gorm:"column:sellerid" json:"sellerid"`
-	Psnapshotid string `gorm:"column:psnapshotid" json:"psnapshotid"`
-	Pdttypeid string `gorm:"column:pdttypeid" json:"pdttypeid"`
-	Pdttypename string `gorm:"column:pdttypename" json:"pdttypename"`
-	Otherpropvalueids string `gorm:"column:otherpropvalueids" json:"otherpropvalueids"`
+	Sellerid string `gorm:"column:sellerId" json:"sellerid"`
+	Psnapshotid string `gorm:"column:pSnapshotId" json:"psnapshotid"`
+	Pdttypeid string `gorm:"column:pdtTypeId" json:"pdttypeid"`
+	Pdttypename string `gorm:"column:pdtTypeName" json:"pdttypename"`
+	Otherpropvalueids string `gorm:"column:otherPropValueIds" json:"otherpropvalueids"`
 	Title string `gorm:"column:title" json:"title"`
-	Sellingpoint string `gorm:"column:sellingpoint" json:"sellingpoint"`
-	Pdtquality string `gorm:"column:pdtquality" json:"pdtquality"`
-	Sourcecode string `gorm:"column:sourcecode" json:"sourcecode"`
-	Sourceplace string `gorm:"column:sourceplace" json:"sourceplace"`
-	Purchasetime time.Time `gorm:"column:purchasetime" json:"purchasetime"`
+	Sellingpoint string `gorm:"column:sellingPoint" json:"sellingpoint"`
+	Pdtquality string `gorm:"column:pdtQuality" json:"pdtquality"`
+	Sourcecode string `gorm:"column:sourceCode" json:"sourcecode"`
+	Sourceplace string `gorm:"column:sourcePlace" json:"sourceplace"`
+	Purchasetime time.Time `gorm:"column:purchaseTime" json:"purchasetime"`
 	Packaged string `gorm:"column:packaged" json:"packaged"`
-	Pdtformat string `gorm:"column:pdtformat" json:"pdtformat"`
+	Pdtformat string `gorm:"column:pdtFormat" json:"pdtformat"`
 	Spec string `gorm:"column:spec" json:"spec"`
-	Minnum float64 `gorm:"column:minnum" json:"minnum"`
+	Minnum float64 `gorm:"column:minNum" json:"minnum"`
 	Price float64 `gorm:"column:price" json:"price"`
-	Shelvetime time.Time `gorm:"column:shelvetime" json:"shelvetime"`
-	Offsaletime time.Time `gorm:"column:offsaletime" json:"offsaletime"`
-	Slideimgs string `gorm:"column:slideimgs" json:"slideimgs"`
-	Inspectstatus string `gorm:"column:inspectstatus" json:"inspectstatus"`
+	Shelvetime time.Time `gorm:"column:shelveTime" json:"shelvetime"`
+	Offsaletime time.Time `gorm:"column:offSaleTime" json:"offsaletime"`
+	Slideimgs string `gorm:"column:slideImgs" json:"slideimgs"`
+	Inspectstatus string `gorm:"column:inspectStatus" json:"inspectstatus"`
 	Status string `gorm:"column:status" json:"status"`
-	Checkstatus string `gorm:"column:checkstatus" json:"checkstatus"`
-	Saleamount float64 `gorm:"column:saleamount" json:"saleamount"`
-	Createby string `gorm:"column:createby" json:"createby"`
-	Createtime time.Time `gorm:"column:createtime" json:"createtime"`
-	Updateby string `gorm:"column:updateby" json:"updateby"`
-	Updatetime time.Time `gorm:"column:updatetime" json:"updatetime"`
+	Checkstatus string `gorm:"column:checkStatus" json:"checkstatus"`
+	Saleamount float64 `gorm:"column:saleAmount" json:"saleamount"`
+	Createby string `gorm:"column:createBy" json:"createby"`
+	Createtime time.Time `gorm:"column:createTime" json:"createtime"`
+	Updateby string `gorm:"column:updateBy" json:"updateby"`
+	Updatetime time.Time `gorm:"column:updateTime" json:"updatetime"`
 	Isdelete string `gorm:"column:isdelete" json:"isdelete"`
 }
 
