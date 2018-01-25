a7a63ba660e0d13094e7b40f0e81dfff83ec4bb3
lj360560179 <360560179@qq.com>
2018-01-25 11:51:29 +0800

    go study

--- controller/redis/redis.go
+++ controller/redis/redis.go
@@ -1,8 +1,22 @@
 package redis
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go_server/model"
+	"go_server/controller/common"
+	"github.com/garyburd/redigo/redis"
+)
 
 func GetRedis(c *gin.Context){
-
-
+	RedisConn := model.RedisPool.Get()
+	defer RedisConn.Close()
+	RedisConn.Send("SET", "foo", "bar")
+	RedisConn.Flush()
+	minuteCount, err := redis.String(RedisConn.Do("GET", "foo"))
+	if err != nil {
+		common.SendErrorMsg(err.Error(),c)
+		return
+	}
+	common.SendResponse(minuteCount,c)
+	return
 }
