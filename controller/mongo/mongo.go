7574995a977ec20dc65f922628a02383f940fccb
lj360560179 <360560179@qq.com>
2018-01-25 10:31:11 +0800

    go study

--- controller/mongo/mongo.go
+++ controller/mongo/mongo.go
@@ -1,7 +1,42 @@
 package mongo
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"fmt"
+	"log"
+)
+
+type Person struct{
+	Name string
+	Phone string
+}
 
 func GetByMo(c *gin.Context){
+	session, err := mgo.Dial("192.168.99.100:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	a := session.DB("test").C("people")
+	err = a.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := Person{}
+	err = a.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
+
 	return
 }
