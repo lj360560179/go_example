e407a22f8f397a6fe7f5ab554b7833a2685fdd1c
lj360560179 <360560179@qq.com>
2018-04-28 17:24:18 +0800

    redigo

--- controller/redis/job.go
+++ controller/redis/job.go
@@ -7,17 +7,19 @@ import (
 
 	"fmt"
 
+	"time"
 )
 
 
 func redisJob()  {
-
 	c := cron.New()
 	spec := "*/5 * * * * ?"
 	c.AddFunc(spec, func() {
 		zset :=getSoredSetByRange("ZSET",0,10,true)
 		for _, set := range zset {
-			fmt.Println(set)
+			if zetScore("ZSET",set) > time.Now().UnixNano(){
+
+			}
 		}
 	})
 
